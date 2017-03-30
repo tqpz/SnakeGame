bd169a65d01cb6556eb4cab7796ca311acb5f6d8
tpqz <tapka511@gmail.com>
2017-03-30 20:14:08 +0200

    added bounds detection, self-crash detection, simple buttons and popup window with score.

--- src/GameClient.java
+++ src/GameClient.java
@@ -1,8 +1,11 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.net.Socket;
@@ -12,32 +15,51 @@ import java.net.Socket;
  */
 public class GameClient extends Application {
 
+    public static void main(String args[]) throws Exception {
+        launch(args);
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Snake");
+        primaryStage.setResizable(false);
+
         BorderPane root = new BorderPane();
-        Scene scene = new Scene(root, 800, 600);
         GameScene gameScene = new GameScene();
+        Pane settingBar = new Pane();
+        root.setPadding(new Insets(10, 20, 10, 20));
+
+        Button reset = new Button("Reset");
+        Button start = new Button("Start");
+
+        Scene scene = new Scene(root, 1000, 620);
+
+        reset.setLayoutX(50);
+
+        settingBar.getChildren().addAll(reset, start);
+
+        start.setOnAction(event -> gameScene.getTimer().start());
+        reset.setOnAction(event -> gameScene.resetAll());
 
         root.setCenter(gameScene);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        root.setRight(settingBar);
+
 
         scene.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> {
-            if (key.getCode() == KeyCode.DOWN)
+            if (key.getCode() == KeyCode.DOWN && gameScene.getLastDirection() != KeyCode.UP)
                 gameScene.setLastDirection(KeyCode.DOWN);
-            else if (key.getCode() == KeyCode.UP)
+            else if (key.getCode() == KeyCode.UP && gameScene.getLastDirection() != KeyCode.DOWN)
                 gameScene.setLastDirection(KeyCode.UP);
-            else if (key.getCode() == KeyCode.LEFT)
+            else if (key.getCode() == KeyCode.LEFT && gameScene.getLastDirection() != KeyCode.RIGHT)
                 gameScene.setLastDirection(KeyCode.LEFT);
-            else if (key.getCode() == KeyCode.RIGHT)
+            else if (key.getCode() == KeyCode.RIGHT && gameScene.getLastDirection() != KeyCode.LEFT)
                 gameScene.setLastDirection(KeyCode.RIGHT);
         });
 
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
         Socket clientSocket = new Socket("localhost", 4444);
         clientSocket.close();
-    }
-
-    public static void main(String args[]) throws Exception {
-        launch(args);
     }
 }
--- src/GameScene.java
+++ src/GameScene.java
@@ -1,51 +1,89 @@
 import javafx.animation.AnimationTimer;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.ConcurrentModificationException;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by Mateusz on 29.03.2017.
  */
 public class GameScene extends Pane {
     private final static int SNAKE_SIZE = 10;
-    private static int ANIMATION_SPEED = 20;
+    private static int ANIMATION_SPEED = 30;
+    boolean crash = false;
     private int snakeLength = 1;
     private int gameSceneWidth;
     private int gameSceneHeight;
+    private AnimationTimer timer;
     private Canvas canvas = new Canvas(gameSceneWidth, gameSceneHeight);
     private GraphicsContext gc = canvas.getGraphicsContext2D();
     private ArrayList<Point> snake;
     private ArrayList<Point> apple;
     private KeyCode lastDirection;
+    private boolean dead = false;
+    private boolean isRunning = false;
     private int posX = 10;
     private int posY = 10;
 
     public GameScene() {
-        gameSceneHeight = 600;
-        gameSceneWidth = 800;
-        snake = new ArrayList<>(snakeLength);
-        snake.add(new Point(posX, posY));
-        apple = new ArrayList<>(0);
-        apple.add(new Point(generateApple()));
+        setInitialState();
         getChildren().add(canvas);
 
-        AnimationTimer timer = new AnimationTimer() {
+        timer = new AnimationTimer() {
             private long lastUpdate = 0;
 
             @Override
             public void handle(long now) {
                 if (now - lastUpdate >= ANIMATION_SPEED * 1_000_000) {
+                    requestLayout();
                     updateScene();
                     lastUpdate = now;
+                    isRunning = true;
                 }
             }
         };
-        timer.start();
+    }
+
+    public AnimationTimer getTimer() {
+        return timer;
+    }
+
+    public void setInitialState() {
+        if (isRunning) {
+            timer.stop();
+            isRunning = false;
+        }
+        isRunning = false;
+
+        posX = 25;
+        posY = 25;
+
+        gameSceneHeight = 600;
+        gameSceneWidth = 800;
+        canvas.setWidth(gameSceneWidth);
+        canvas.setHeight(gameSceneHeight);
+
+        snake = new ArrayList<>(snakeLength);
+        snake.add(new Point(posX, posY));
+        apple = new ArrayList<>(0);
+        apple.add(generateApple());
+
+        dead = false;
+        ANIMATION_SPEED = 30;
+        snakeLength = 100;
+    }
+
+    public KeyCode getLastDirection() {
+        return lastDirection;
     }
 
     public void setLastDirection(KeyCode lastDirection) {
@@ -53,80 +91,106 @@ public class GameScene extends Pane {
     }
 
     public void updateScene() {
-        boolean[][] cellsBoard = new boolean[gameSceneWidth / SNAKE_SIZE + 2][gameSceneHeight / SNAKE_SIZE + 2];
+        if (!dead) {
+            boolean[][] cellsBoard = new boolean[gameSceneWidth / SNAKE_SIZE][gameSceneHeight / SNAKE_SIZE];
+            try {
+                for (int i = 0; i < snake.size(); i++) {
+                    cellsBoard[snake.get(i).x + 1][snake.get(i).y + 1] = true;
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                timer.stop();
+                dead = true;
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Score");
+                alert.setHeaderText("You died!");
+                alert.setContentText("Your score: " + (snakeLength - 1));
+                alert.show();
 
-        try {
-            for (int i = 0; i < snake.size(); i++) {
-                cellsBoard[snake.get(i).x + 1][snake.get(i).y + 1] = true;
+            } catch (NullPointerException e) {
+                System.out.println("Nullpointer at thread start");
             }
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Out of bounds at thread start");
-        } catch (NullPointerException e) {
-            System.out.println("Nullpointer at thread start");
-        }
 
-        try {
-            //Thread.sleep(ANIMATION_SPEED);
-            if (snake.get(snake.size() - 1).equals(apple.get(0))) {
-                snakeLength++;
-                apple.clear();
-                apple.add(generateApple());
+            try {
+                if (snake.get(snake.size() - 1).equals(apple.get(0))) {
+                    snakeLength++;
+                    ANIMATION_SPEED--;
+                    apple.clear();
+                    apple.add(generateApple());
+                }
+                move();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            move();
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
     private Point generateApple() {
-        int x = (int) (Math.random() * gameSceneWidth / SNAKE_SIZE);
-        int y = (int) (Math.random() * gameSceneHeight / SNAKE_SIZE);
+        int x = (int) (Math.random() * (canvas.getWidth() - SNAKE_SIZE) / SNAKE_SIZE);
+        int y = (int) (Math.random() * (canvas.getHeight() - SNAKE_SIZE) / SNAKE_SIZE);
         return new Point(x, y);
     }
 
-    public void move() {
-        gc.clearRect(0, 0, getWidth(), getHeight());
-        if (lastDirection == KeyCode.DOWN) {
-            if (snake.size() >= snakeLength) {
-                snake.remove(0);
-            }
-            posY += 1;
-            snake.add(new Point(posX, posY));
-            requestLayout();
+    public void resetAll() {
+        setInitialState();
+        requestLayout();
+        crash = false;
+        //timer.start();
+    }
 
-        } else if (lastDirection == KeyCode.UP) {
-            if (snake.size() >= snakeLength) {
-                snake.remove(0);
-            }
-            posY -= 1;
-            snake.add(new Point(posX, posY));
-            requestLayout();
+    private void move() {
 
-        } else if (lastDirection == KeyCode.LEFT) {
-            if (snake.size() >= snakeLength) {
-                snake.remove(0);
-            }
-            posX -= 1;
-            snake.add(new Point(posX, posY));
-            requestLayout();
+        if (!crash) {
+            Set<Point> set = new HashSet<>(snake);
 
-        } else if (lastDirection == KeyCode.RIGHT) {
-            if (snake.size() >= snakeLength) {
-                snake.remove(0);
+            if (set.size() < snake.size()) {
+                crash = true;
             }
-            posX += 1;
-            snake.add(new Point(posX, posY));
-            requestLayout();
+
+            if (lastDirection == KeyCode.DOWN) {
+                if (snake.size() >= snakeLength) {
+                    snake.remove(0);
+                }
+                posY += 1;
+                snake.add(new Point(posX, posY));
+                requestLayout();
+
+            } else if (lastDirection == KeyCode.UP) {
+                if (snake.size() >= snakeLength) {
+                    snake.remove(0);
+                }
+                posY -= 1;
+                snake.add(new Point(posX, posY));
+                requestLayout();
+
+            } else if (lastDirection == KeyCode.LEFT) {
+                if (snake.size() >= snakeLength) {
+                    snake.remove(0);
+                }
+                posX -= 1;
+                snake.add(new Point(posX, posY));
+                requestLayout();
+
+            } else if (lastDirection == KeyCode.RIGHT) {
+                if (snake.size() >= snakeLength) {
+                    snake.remove(0);
+                }
+                posX += 1;
+                snake.add(new Point(posX, posY));
+                requestLayout();
+            }
         }
     }
 
-    protected void layoutChildren() {
-        super.layoutChildren();
-        canvas.setWidth(gameSceneWidth - SNAKE_SIZE + 1);
-        canvas.setHeight(gameSceneHeight - SNAKE_SIZE + 1);
-        gc.setLineWidth(0.2);
+    public void drawCanvasFrame() {
+        gc.strokeLine(0, canvas.getHeight(), canvas.getWidth(), canvas.getHeight());
+        gc.strokeLine(canvas.getWidth(), 0, canvas.getWidth(), canvas.getHeight());
+        gc.strokeLine(0, 0, 0, canvas.getHeight());
+        gc.strokeLine(0, 0, canvas.getWidth(), 0);
+    }
 
+    private void drawSnake() {
         try {
+            gc.setFill(Color.GREEN);
             for (Point newPoint : snake) {
                 gc.fillRect(SNAKE_SIZE + (SNAKE_SIZE * newPoint.x),
                         SNAKE_SIZE + (SNAKE_SIZE * newPoint.y),
@@ -136,28 +200,31 @@ public class GameScene extends Pane {
         } catch (ConcurrentModificationException e) {
         } catch (NullPointerException e) {
         }
+    }
 
+    private void drawApple() {
         try {
+            Image img = new Image("img/apple.png");
             for (Point newPoint : apple) {
-                gc.fillRect(SNAKE_SIZE + (SNAKE_SIZE * newPoint.x),
-                        SNAKE_SIZE + (SNAKE_SIZE * newPoint.y),
-                        SNAKE_SIZE,
-                        SNAKE_SIZE);
+                gc.drawImage(img, SNAKE_SIZE + ((SNAKE_SIZE * newPoint.x) - 3), SNAKE_SIZE + (SNAKE_SIZE * newPoint.y) - 3);
+
             }
         } catch (ConcurrentModificationException e) {
         } catch (NullPointerException e) {
         }
+    }
 
+    private void drawGrid() {
         try {
-            for (int i = 0; i <= gameSceneWidth / SNAKE_SIZE; i++) {
+            for (int i = 0; i < canvas.getWidth() / SNAKE_SIZE; i++) {
                 gc.strokeLine(((i * SNAKE_SIZE) + SNAKE_SIZE),
-                        SNAKE_SIZE,
+                        0,
                         (i * SNAKE_SIZE) + SNAKE_SIZE,
                         SNAKE_SIZE + (SNAKE_SIZE * gameSceneHeight / SNAKE_SIZE));
             }
 
-            for (int i = 0; i <= gameSceneHeight / SNAKE_SIZE; i++) {
-                gc.strokeLine(SNAKE_SIZE,
+            for (int i = 0; i < canvas.getHeight() / SNAKE_SIZE - 1; i++) {
+                gc.strokeLine(0,
                         ((i * SNAKE_SIZE) + SNAKE_SIZE),
                         SNAKE_SIZE * (gameSceneWidth / SNAKE_SIZE),
                         ((i * SNAKE_SIZE) + SNAKE_SIZE));
@@ -165,5 +232,16 @@ public class GameScene extends Pane {
 
         } catch (NullPointerException e) {
         }
+    }
+
+    protected void layoutChildren() {
+        super.layoutChildren();
+        gc.clearRect(0, 0, getWidth(), getHeight());
+        gc.setLineWidth(0.2);
+
+        drawCanvasFrame();
+        drawSnake();
+        drawApple();
+        drawGrid();
     }
 }
